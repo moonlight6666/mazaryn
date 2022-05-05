d19d42734bb6cd225e9c27c5a40739ed6cafe204
Emilano <azadvararvand@gmail.com>
2022-05-05 14:12:34 +0430

    Add files via upload

---
+++ lib/mazaryn/info/event.ex
@@ -0,0 +1,8 @@
+defmodule Event do
+  defstruct [
+    name: nil,
+    date: nil,
+    loc: nil,
+    desc: nil
+  ]
+end
---
+++ lib/mazaryn/info/user.ex
@@ -0,0 +1,8 @@
+defmodule User do
+  defstruct [
+    id: nil,
+    username: nil,
+    password: nil,
+    email: nil
+  ]
+end
